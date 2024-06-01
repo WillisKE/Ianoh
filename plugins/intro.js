881858b98fefe8c97fb6726a3407f7bd7e92b796
Willis <130226179+WillisKE@users.noreply.github.com>
2024-06-01 20:48:12 +0300

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -66,7 +66,7 @@ let text = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
  //---------------------------------------------------------------------------
  smd({
              pattern: "willis",
-             alias : ["intro","willis"]
+             alias : ["intro","willis"],
              desc: "Show intro of user",
              category: "whatsapp",
              filename: __filename,
